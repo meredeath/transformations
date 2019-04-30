7c9a57458c5a0f24022f9b02020d094a8f0b8c00
Meredith Hu <humeiyin@gmail.com>
2019-04-30 00:38:28 -0400

    added some comments to main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from matrix import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-edges = []
-transform = new_matrix()
+edges = [] #single edge matrix
+transform = new_matrix() #single master transformation matrix
 
 parse_file( 'script', edges, transform, screen, color )
